caa977db976622fe3955ad345267d7c06cbecf29
Oliver Adams <oliver.adams@gmail.com>
2020-11-12 20:35:54 +1100

    Refactoring.

--- pyramid.py
+++ pyramid.py
@@ -1,3 +1,5 @@
+import argparse
+
 import pandas as pd
 import plotnine as p9
 
@@ -12,10 +14,16 @@ NOT_ON = THECRAG_NOT_ON.union({'Tick', 'Aid solo', 'Top rope', 'Second'})
 
 
 def clean_free(ascent_type):
+    """ Returns true if an ascent type is clean and free
+        - Clean: The rope was not weighted (toproping is acceptable)
+        - Free: Not aid climbing
+    """
     return ascent_type not in NOT_ON
 
 
 def is_ewbanks(ascent_grade):
+    """ If a grade can be converted to an integer, then it must be in the
+    Ewbanks system, or at least not French or YDS."""
     try:
         int(ascent_grade)
     except ValueError:
@@ -24,30 +32,39 @@ def is_ewbanks(ascent_grade):
         return True
 
 
-def prepare_df():
-    df = pd.read_csv('OLIVERADAMS-logbook-2020-11-12.csv')
+def prepare_df(df):
 
     # Get all clean free ascents (not weighting the rope)
-
     df = df[df['Ascent Type'].apply(clean_free)]
 
     # Temporarily: just removing duplicate routes so we can just plot a pyramid
-    # for clean ascents
+    # for clean ascents. When we want to plot different ascent types with
+    # different colours, this will break down because it might not choose the
+    # best ascent type (e.g. it might prune out a flash in favour of a
+    # red-point
     df = df.drop_duplicates(['Route ID'])
+    # TODO Remove repeats by taking the best ascent.
 
-    # Remove non-ewbanks graded stuff.
+    # Remove non-Ewbanks graded stuff.
     df = df[df['Ascent Grade'].apply(is_ewbanks)]
-
+    # Convert remaining grades to Ewbanks
     df['Ewbanks Grade'] = df['Ascent Grade'].apply(lambda x: int(x))
 
-    # Remove repeats by taking the best ascent.
+    # TODO Break stats down by pitches for more fine-grained info. Currently
+    # just using whole routes but I would prefer it if the output was per-pitch
 
-    # Break stats down by pitches for more fine-grained info.
-
-    # Separate trad from sport and bouldering
+    # TODO Separate trad from sport and bouldering
 
     return df
 
 
-df = prepare_df()
-p9.ggplot(df) + p9.geom_bar(p9.aes(x='Grade'))
+parser = argparse.ArgumentParser()
+parser.add_argument('csv', help='Your logbook from thecrag.com in CSV format.')
+
+# How about we try doing all the IO here and make all our functions pure?
+if __name__ == '__main__':
+    args = parser.parse_args()
+    df = pd.read_csv(args.csv)
+    df = prepare_df(df)
+    plot = [p9.ggplot(df) + p9.geom_bar(p9.aes(x='Ewbanks Grade'))]
+    p9.save_as_pdf_pages(plot)
