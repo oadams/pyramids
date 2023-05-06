bec98a5bdb445a6adf9e8d6e14a9d924dd865c8d
Oliver Adams <oliver.adams@gmail.com>
2023-05-07 09:55:35 +1000

    Some cleaning of dash_pyramid.py

--- dash_pyramid.py
+++ dash_pyramid.py
@@ -18,6 +18,7 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+# Define the layout that is present when the page is opened
 app.layout = html.Div([
     dcc.Upload(
         id='upload-data',
@@ -63,78 +64,91 @@ app.layout = html.Div([
     ),
 ])
 
+# A mapping from ascent types to colours
 COLOR_MAP = {
-    "Trad onsight": "#036611",
-    "Onsight solo": "#036611",
-    "Sport onsight": "#06b91f",
-    "Second onsight": "#07db24",
-    "Top rope onsight": "#06fc28",
-    "Trad flash": "#FF6600",
-    "Sport flash": "#FF9900",
-    "Second flash": "#ffcc00",
-    "Top rope flash": "#ffff00",
-    "Trad red point": "#990000",
-    "Solo": "#990000",
-    "Sport red point": "#ff0000",
-    "Red point": "#ff0000",
-    "Ground up red point": "#ff1188",
-    "Pink point": "#ff33cc",
-    "Second clean": "#cc66ff",
-    "Top rope clean": "#6666ff",
-    "Roped Solo": "#3333cc",
-    "Aid": "#000066",
-    "Aid solo": "#000066",
-    "Trad lead with rest": "#666666",
-    "Sport lead with rest": "#666666",
-    "Hang dog": "#666666",
-    "Second with rest": "#999999",
-    "Top rope with rest": "#999999",
-    "All free with rest": "#999999",
-    "Attempt": "#cccccc",
-    "Trad attempt": "#cccccc",
-    "Sport attempt": "#cccccc",
-    "Second attempt": "#cccccc",
-    "Top rope attempt": "#cccccc",
-    "Retreat": "#cccccc",
-    "Working": "#cccccc",
-    "Clean": "#6666ff",
-    "Tick": "#66cccc",
+    'Trad onsight': '#036611',
+    'Onsight solo': '#036611',
+    'Sport onsight': '#06b91f',
+    'Second onsight': '#07db24',
+    'Top rope onsight': '#06fc28',
+    'Trad flash': '#FF6600',
+    'Sport flash': '#FF9900',
+    'Second flash': '#ffcc00',
+    'Top rope flash': '#ffff00',
+    'Trad red point': '#990000',
+    'Solo': '#990000',
+    'Sport red point': '#ff0000',
+    'Red point': '#ff0000',
+    'Ground up red point': '#ff1188',
+    'Pink point': '#ff33cc',
+    'Second clean': '#cc66ff',
+    'Top rope clean': '#6666ff',
+    'Roped Solo': '#3333cc',
+    'Aid': '#000066',
+    'Aid solo': '#000066',
+    'Trad lead with rest': '#666666',
+    'Sport lead with rest': '#666666',
+    'Hang dog': '#666666',
+    'Second with rest': '#999999',
+    'Top rope with rest': '#999999',
+    'All free with rest': '#999999',
+    'Attempt': '#cccccc',
+    'Trad attempt': '#cccccc',
+    'Sport attempt': '#cccccc',
+    'Second attempt': '#cccccc',
+    'Top rope attempt': '#cccccc',
+    'Retreat': '#cccccc',
+    'Working': '#cccccc',
+    'Clean': '#6666ff',
+    'Tick': '#66cccc',
+    #'Lead': '#66cccc',
+    #'Second': '#66cccc',
+    #'Top rope': '#66cccc',
 }
 
-def parse_contents(contents, filename, date, unique, route_gear_style, ascent_gear_style,
+
+def parse_contents(contents, filename, unique, route_gear_style, ascent_gear_style,
                    start_date, end_date, free):
-    content_type, content_string = contents.split(',')
+    """ Function that preprocesses the dataframe according to the various other options.  """
+    _, content_string = contents.split(',')
 
     decoded = base64.b64decode(content_string)
     try:
         # Assume that the user uploaded a CSV file
         df = pd.read_csv(
             io.StringIO(decoded.decode('utf-8')))
-    except Exception as e:
+    except Exception as e: # TODO Make this exception less general.
         print(e)
         return html.Div([
             'There was an error processing this file.'
         ])
 
-    df = prepare_df(df, unique=unique, route_gear_style=route_gear_style, ascent_gear_style=ascent_gear_style, start_date=start_date,
-                    end_date=end_date, free_only=(free=='Free only'))
+    df = prepare_df(df, unique=unique, route_gear_style=route_gear_style,
+                    ascent_gear_style=ascent_gear_style, start_date=start_date,
+                    end_date=end_date, free_only=(free == 'Free only'))
 
-    df = df.drop(['Ascent Label', 'Ascent ID', 'Ascent Link', 'Ascent Grade', 'Route Gear Style', 'Ascent Height', 'Route Height', 'Country Link', 'Crag Link'], axis=1)
+    df = df.drop(['Ascent Label', 'Ascent ID', 'Ascent Link', 'Ascent Grade', 'Route Gear Style',
+                  'Ascent Height', 'Route Height', 'Country Link', 'Crag Link'], axis=1)
 
+    # Line wrap the comment so that the mouseovers don't expand to fill the width of the page
     df['Comment'] = df['Comment'].apply(lambda x: '<br>'.join(textwrap.wrap(str(x))))
 
+    # I tried making COLOR_MAP a defaultdict that backed off to #66cccc but for some reason the bar
+    # chart did not use those values.
     color_map = {}
     for ascent_type in df['Ascent Type'].unique():
         if ascent_type in COLOR_MAP:
             color_map[ascent_type] = COLOR_MAP[ascent_type]
         else:
-            color_map[ascent_type] = "#66cccc"
-    fig = px.bar(df, x='num', y='Ewbanks Grade', color='Ascent Type', orientation='h',
-                 hover_data=['Country', 'Crag Name', "Route Name", 'Ascent Date', "Comment"],
-                 color_discrete_map=color_map,
-            labels = {'num': 'Number of ascents'},
-    )
+            color_map[ascent_type] = '#66cccc'
 
+    fig = px.bar(df, x='num', y='Ewbanks Grade', color='Ascent Type', orientation='h',
+                 hover_data=['Country', 'Crag Name', 'Route Name', 'Ascent Date', 'Comment'],
+                 color_discrete_map=color_map,
+                 labels={'num': 'Number of ascents'})
+
+    # The layout when you mouse over an ascent tile. `customdata` gives access to the bar chart's
+    # hover_data.
     fig.update_traces(hovertemplate=('Ascent Date: %{customdata[3]}<br>'
                                      'Route Name: %{customdata[2]}<br>'
                                      'Crag Name: %{customdata[1]}<br>'
@@ -142,15 +156,15 @@ def parse_contents(contents, filename, date, unique, route_gear_style, ascent_ge
                                      'Comment: %{customdata[4]}'))
 
     fig.update_layout(
-        yaxis = dict(
-            tickmode = 'linear',
-            tick0 = 1,
-            dtick = 1,
+        yaxis=dict(
+            tickmode='linear',
+            tick0=1,
+            dtick=1,
         ),
-        xaxis = dict(
-            tickmode = 'linear',
-            tick0 = 0,
-            dtick = 5,
+        xaxis=dict(
+            tickmode='linear',
+            tick0=0,
+            dtick=5,
         )
     )
 
@@ -162,8 +176,6 @@ def parse_contents(contents, filename, date, unique, route_gear_style, ascent_ge
 
     return html.Div([
         html.H5(filename),
-        #dcc.RadioItems(['Unique', 'Duplicates'], 'Unique'),
-        #dash_table.DataTable(data=df.to_dict('records'), page_size=10),
         html.B(f'Number of climbs: {len(df)}'),
         dcc.Graph(figure=fig, config=config)
     ])
@@ -171,27 +183,33 @@ def parse_contents(contents, filename, date, unique, route_gear_style, ascent_ge
 @app.callback(Output('date-range', 'start_date'),
               Output('date-range', 'end_date'),
               Input('clear-dates', 'n_clicks'))
-def clear_dates(_clear_btn_n_clicks):
+def clear_dates(_):
+    """ When the clear date button is clicked, feed None dates into the date picker to reset it. """
     return None, None
+
 
 @app.callback(Output('output-data-upload', 'children'),
               Input('upload-data', 'contents'),
               State('upload-data', 'filename'),
-              State('upload-data', 'last_modified'),
               Input('unique-radio', 'value'),
               Input('route-gear-style', 'value'),
               Input('ascent-gear-style', 'value'),
               Input('date-range', 'start_date'),
               Input('date-range', 'end_date'),
               Input('free-ascent', 'value'))
-def update_output(content, name, modified, unique, route_gear_style,
+def update_output(content, name, unique, route_gear_style,
                   ascent_gear_style, start_date, end_date, free):
+    """ Any time the radio buttons or upload component is changed, handle it and return components
+    to render. """
+    children = []
     if content is not None:
-        children = [
-            parse_contents(content, name, modified, unique, route_gear_style, ascent_gear_style, start_date,
+        children.append(
+            parse_contents(content, name, unique,
+                           route_gear_style, ascent_gear_style,
+                           start_date,
                            end_date, free)
-        ]
-        return children
+        )
+    return children
 
 if __name__ == '__main__':
     app.run_server(debug=True)
